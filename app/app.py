8ead0d46650c7aba244b2ed8c2d20b633fd82316
Clayton <clayton.aa@gmail.com>
2018-02-08 08:39:10 -0300

    Adicionado template inicial de login

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 """ Aplicacao principal
 """
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 from flaskext.markdown import Markdown
 
 from .database import db
@@ -15,6 +15,7 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
     register_extensions(app)
+    register_routes(app)
     register_blueprints(app)
     register_formatters(app)
     return app
@@ -29,3 +30,11 @@ def register_blueprints(app):
     app.register_blueprint(chamados_blueprint, url_prefix="/chamados")
     app.register_blueprint(excecoes_blueprint, url_prefix="/excecoes")
     app.register_blueprint(graficos_blueprint, url_prefix="/graficos")
+
+
+def login_route():
+    return render_template("login.html")
+
+
+def register_routes(app):
+    app.add_url_rule("/login", view_func=login_route)
